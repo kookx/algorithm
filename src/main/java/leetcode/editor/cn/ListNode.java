5d3fd8e4ffcc353270f9efce1dc09879e5e25dc0
kookx <onekook.me@gmail.com>
2020-07-16 22:49:03 +0800

    新增ListNode

---
+++ src/main/java/leetcode/editor/cn/ListNode.java
@@ -0,0 +1,12 @@
+package leetcode.editor.cn;
+
+/**
+ * @Description TODO
+ * @Date 2020/7/15 10:11 AM
+ * @Created by kook<onekook.me@gmail.com>
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
