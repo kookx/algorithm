61db7f2720b6872ce876f51515e3320a7039e5eb
kookx <onekook.me@gmail.com>
2020-07-18 18:39:04 +0800

    $1 #42.接雨水 @算法训练营课后作业

---
+++ src/main/java/leetcode/editor/cn/P42TrappingRainWater.java
@@ -0,0 +1,50 @@
+//给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。 
+//
+// 
+//
+// 上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 感谢 Mar
+//cos 贡献此图。 
+//
+// 示例: 
+//
+// 输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+//输出: 6 
+// Related Topics 栈 数组 双指针 
+// 👍 1470 👎 0
+
+ 
+package leetcode.editor.cn;
+//Java：接雨水
+public class P42TrappingRainWater{
+    public static void main(String[] args) {
+        Solution solution = new P42TrappingRainWater().new Solution();
+        // TO TEST
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int trap(int[] height) {
+        // 解法1.动态规划，暂时没有思路，背诵一遍
+        int sum = 0;
+        int[] max_left = new int[height.length];
+        int[] max_right = new int[height.length];
+
+        for (int i = 1; i < height.length - 1; i++) {
+            max_left[i] = Math.max(max_left[i - 1], height[i - 1]);
+        }
+        for (int i = height.length - 2; i >= 0; i--) {
+            max_right[i] = Math.max(max_right[i + 1], height[i + 1]);
+        }
+        for (int i = 1; i < height.length - 1; i++) {
+            int min = Math.min(max_left[i], max_right[i]);
+            if (min > height[i]) {
+                sum = sum + (min - height[i]);
+            }
+        }
+        return sum;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
