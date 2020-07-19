4ff6a657033c9678aecb0a4995b30e244301db88
kookx <onekook.me@gmail.com>
2020-07-19 21:14:44 +0800

    $1 #312.戳气球 @每日一题

---
+++ src/main/java/leetcode/editor/cn/P312BurstBalloons.java
@@ -0,0 +1,63 @@
+//有 n 个气球，编号为0 到 n-1，每个气球上都标有一个数字，这些数字存在数组 nums 中。 
+//
+// 现在要求你戳破所有的气球。如果你戳破气球 i ，就可以获得 nums[left] * nums[i] * nums[right] 个硬币。 这里的 lef
+//t 和 right 代表和 i 相邻的两个气球的序号。注意当你戳破了气球 i 后，气球 left 和气球 right 就变成了相邻的气球。 
+//
+// 求所能获得硬币的最大数量。 
+//
+// 说明: 
+//
+// 
+// 你可以假设 nums[-1] = nums[n] = 1，但注意它们不是真实存在的所以并不能被戳破。 
+// 0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100 
+// 
+//
+// 示例: 
+//
+// 输入: [3,1,5,8]
+//输出: 167 
+//解释: nums = [3,1,5,8] --> [3,5,8] -->   [3,8]   -->  [8]  --> []
+//     coins =  3*1*5      +  3*5*8    +  1*3*8      + 1*8*1   = 167
+// 
+// Related Topics 分治算法 动态规划 
+// 👍 350 👎 0
+
+ 
+package leetcode.editor.cn;
+
+import java.util.Map;
+
+//Java：戳气球
+public class P312BurstBalloons{
+    public static void main(String[] args) {
+        Solution solution = new P312BurstBalloons().new Solution();
+        // TO TEST
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxCoins(int[] nums) {
+        // 解法1.动态规划,暂时不会，背诵
+        int n = nums.length;
+        int[][] rec = new int[n + 2][n + 2];
+        int[] val = new int[n + 2];
+        val[0] = val[n + 1] = 1;
+        for (int i = 1; i<=n; i++) {
+            val[i] =nums[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--){
+            for (int j = i + 2; j <= n + 1; j++) {
+                for (int k = i + 1; k < j; k++) {
+                    int sum = val[i] * val[k] * val[j];
+                    sum += rec[i][k] + rec[k][j];
+                    rec[i][j] = Math.max(rec[i][j], sum);
+                }
+            }
+        }
+        return rec[0][n + 1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
