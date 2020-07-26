3f742252bf2868b2ec3d72237a9aea21a853548f
kookx <onekook.me@gmail.com>
2020-07-26 10:22:25 +0800

    $1 #329.矩阵中的最长递增路径 @每日一题

---
+++ src/main/java/leetcode/editor/cn/P329LongestIncreasingPathInAMatrix.java
@@ -0,0 +1,78 @@
+//给定一个整数矩阵，找出最长递增路径的长度。 
+//
+// 对于每个单元格，你可以往上，下，左，右四个方向移动。 你不能在对角线方向上移动或移动到边界外（即不允许环绕）。 
+//
+// 示例 1: 
+//
+// 输入: nums = 
+//[
+//  [9,9,4],
+//  [6,6,8],
+//  [2,1,1]
+//] 
+//输出: 4 
+//解释: 最长递增路径为 [1, 2, 6, 9]。 
+//
+// 示例 2: 
+//
+// 输入: nums = 
+//[
+//  [3,4,5],
+//  [3,2,6],
+//  [2,2,1]
+//] 
+//输出: 4 
+//解释: 最长递增路径是 [3, 4, 5, 6]。注意不允许在对角线方向上移动。
+// 
+// Related Topics 深度优先搜索 拓扑排序 记忆化 
+// 👍 236 👎 0
+
+ 
+package leetcode.editor.cn;
+//Java：矩阵中的最长递增路径
+public class P329LongestIncreasingPathInAMatrix{
+    public static void main(String[] args) {
+        Solution solution = new P329LongestIncreasingPathInAMatrix().new Solution();
+        // TO TEST
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public int rows, columns;
+
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        rows = matrix.length;
+        columns = matrix[0].length;
+        int[][] memo = new int[rows][columns];
+        int ans = 0;
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+                ans = Math.max(ans, dfs(matrix, i, j, memo));
+            }
+        }
+        return ans;
+    }
+
+    private int dfs(int[][] matrix, int row, int column, int[][] memo) {
+        if (memo[row][column] != 0) {
+            return memo[row][column];
+        }
+        ++memo[row][column];
+        for (int[] dir : dirs) {
+            int newRow = row + dir[0], newColumn = column + dir[1];
+            if (newRow >= 0 && newRow < rows && newColumn >= 0 && newColumn < columns
+            && matrix[newRow][newColumn] > matrix[row][column]) {
+                memo[row][column] = Math.max(memo[row][column], dfs(matrix, newRow, newColumn, memo) + 1);
+            }
+        }
+        return memo[row][column];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
