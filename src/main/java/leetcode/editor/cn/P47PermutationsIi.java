1198322a62eb0d0baa7b8b27f0c0be77e8bdfe68
kookx <onekook.me@gmail.com>
2020-08-07 21:44:35 +0800

    $2 #47.全排列 II @分类刷题 &数组、回溯算法

--- src/main/java/leetcode/editor/cn/P47PermutationsIi.java
+++ src/main/java/leetcode/editor/cn/P47PermutationsIi.java
@@ -24,6 +24,7 @@ public class P47PermutationsIi{
     public static void main(String[] args) {
         Solution solution = new P47PermutationsIi().new Solution();
         // TO TEST
+        solution.permuteUnique(new int[]{1,1,2});
     }
     
 //leetcode submit region begin(Prohibit modification and deletion)
@@ -31,37 +32,42 @@ class Solution {
     // 解法1. 回溯
     // 思路：步骤同全排列I，剪枝重复出现的情况,用used记录每个数是否用过
     List<List<Integer>> output;
+    int len;
     boolean[] used;
     public List<List<Integer>> permuteUnique(int[] nums) {
         output = new ArrayList<>();
-        used = new boolean[nums.length];
+        if (nums == null || nums.length == 0) {
+            return output;
+        }
+        len = nums.length;
+        used = new boolean[len];
         Arrays.sort(nums);
-        dfs(nums, 0, new ArrayList<>());
+        dfs(nums, new ArrayList<>());
         return output;
     }
 
-    private void dfs(int[] nums, int begin, ArrayList<Integer> temp) {
+    private void dfs(int[] nums, ArrayList<Integer> temp) {
         if (temp.size() == nums.length) {
             output.add(new ArrayList<>(temp));
             return;
         }
 
-        for (int i = begin; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (used[i]) {
                 continue;
             }
             // 如果当前元素和前一个元素相等，并且该元素处于同一层函数for的不同位置
-            if (i > begin && nums[i] == nums[i - 1] && !used[i - 1]) {
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
                 continue;
             }
 
             temp.add(nums[i]);
             used[i] = true;
 
-            dfs(nums, 0, temp);
+            dfs(nums, temp);
 
-            temp.remove(temp.size() - 1);
             used[i] = false;
+            temp.remove(temp.size() - 1);
         }
 
     }
