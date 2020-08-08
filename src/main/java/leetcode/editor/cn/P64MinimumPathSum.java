28bfdba4ff2099b3c54bfce5303bf7e195c5423e
kookx <onekook.me@gmail.com>
2020-08-08 15:52:19 +0800

    $2 #64.最小路径和 @算法训练营课后作业 &数组、动态规划

--- src/main/java/leetcode/editor/cn/P64MinimumPathSum.java
+++ src/main/java/leetcode/editor/cn/P64MinimumPathSum.java
@@ -29,32 +29,27 @@ public class P64MinimumPathSum{
 class Solution {
     // 解法1. 动态规划
     public int minPathSum(int[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+        // 状态定义 dp[i][j] 为ixj网格的最小路径和
+        // 方程： dp[i][j] = min(dp[i-1][j],dp[i][j-1]) + grid[i][j]
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
             return 0;
-        }
-        int row = grid.length, column = grid[0].length;
-        // 定义一个dp数组 保存前面的最优解：最小路径和
-        int dp[][] = new int[row][column];
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
 
         dp[0][0] = grid[0][0];
-
-        // 只含一列的情况
-        for (int i = 1; i < row; ++i) {
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i-1] + grid[0][i];
         }
-        // 只含一行的情况
-        for (int j = 1; j < column; ++j) {
-            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i-1][0] + grid[i][0];
         }
-
-        // 在m n中找
-        for (int i = 1; i < row; ++i) {
-            for (int j = 1; j < column; ++j) {
-                dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1]) + grid[i][j];
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
         }
-
-        return dp[row -1][column -1];
+        return dp[m - 1][n - 1];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
