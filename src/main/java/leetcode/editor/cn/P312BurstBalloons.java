0837695198e6b7598093db4b21b32674b1c1a5fe
kookx <onekook.me@gmail.com>
2020-08-09 14:38:44 +0800

    $2 #312.戳气球 @算法训练营课后作业 &数组、动态规划

--- src/main/java/leetcode/editor/cn/P312BurstBalloons.java
+++ src/main/java/leetcode/editor/cn/P312BurstBalloons.java
@@ -25,8 +25,6 @@
  
 package leetcode.editor.cn;
 
-import java.util.Map;
-
 //Java：戳气球
 public class P312BurstBalloons{
     public static void main(String[] args) {
@@ -37,25 +35,70 @@ public class P312BurstBalloons{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int maxCoins(int[] nums) {
-        // 解法1.动态规划,暂时不会，背诵
-        int n = nums.length;
-        int[][] rec = new int[n + 2][n + 2];
-        int[] val = new int[n + 2];
-        val[0] = val[n + 1] = 1;
-        for (int i = 1; i<=n; i++) {
-            val[i] =nums[i - 1];
+        return dp(nums);
+    }
+
+    // 解法1.回溯算法
+    // 思路：穷举所有可能戳破气球的顺序，获取最大值，类似于《全排列》问题
+    private int backtrack(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] a = new int[nums.length+2];
+        a[0] = 1;
+        a[a.length-1] = 1;
+        for(int i = 0; i < nums.length; i++){
+            a[i+1] = nums[i];
+        }
+        return helper(a, 0, a.length-1, new Integer[a.length][a.length]);
+    }
+
+    // 每个回溯返回(i, j)区间内所能获得最大coins数
+    public int helper(int[] a, int i, int j, Integer[][] memo){
+        // 如果记忆有，直接返回
+        if(memo[i][j] != null){
+            return memo[i][j];
+        }
+        int max = 0;
+
+        // 注意每次从(i, j)中取一个气球，但不包括i和j
+        // 穷尽所有放置方案，得到一个最大的方案max
+        for(int k = i+1; k < j; k++){
+            max = Math.max(max,
+                    helper(a, i, k, memo) +
+                            a[i] * a[k] * a[j]+
+                            helper(a, k, j, memo));
         }
 
-        for (int i = n - 1; i >= 0; i--){
-            for (int j = i + 2; j <= n + 1; j++) {
+        // 记忆
+        return memo[i][j] = max;
+    }
+
+    // 解法2.动态规划
+    private int dp(int[] nums) {
+        int n = nums.length;
+        int[] points = new int[n + 2];
+        points[0] = points[n + 1] = 1;
+        for (int i = 1; i <= n; i++) {
+            points[i] = nums[i - 1];
+        }
+        // 初始化值为0
+        int[][] dp = new int[n + 2][n + 2];
+        // i 应该从下往上
+        for (int i = n; i >= 0; i--) {
+            // j 应该从左往右
+            for (int j = i + 1; j < n + 2; j++) {
+                // 最后戳破的气球是哪个？
                 for (int k = i + 1; k < j; k++) {
-                    int sum = val[i] * val[k] * val[j];
-                    sum += rec[i][k] + rec[k][j];
-                    rec[i][j] = Math.max(rec[i][j], sum);
+                    // 择优做选择
+                    dp[i][j] = Math.max(
+                            dp[i][j],
+                            dp[i][k] + dp[k][j] + points[i]*points[j]*points[k]
+                    );
                 }
             }
         }
-        return rec[0][n + 1];
+        return dp[0][n + 1];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
