84b2a7f070dc0dc06321df8b8cfcd4fa7ac6d8b8
kookx <onekook.me@gmail.com>
2020-08-11 19:47:09 +0800

    $1 #130.被围绕的区域 @每日一题 &数组、DFS、并查集

---
+++ src/main/java/leetcode/editor/cn/P130SurroundedRegions.java
@@ -0,0 +1,84 @@
+//给定一个二维的矩阵，包含 'X' 和 'O'（字母 O）。 
+//
+// 找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。 
+//
+// 示例: 
+//
+// X X X X
+//X O O X
+//X X O X
+//X O X X
+// 
+//
+// 运行你的函数后，矩阵变为： 
+//
+// X X X X
+//X X X X
+//X X X X
+//X O X X
+// 
+//
+// 解释: 
+//
+// 被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，或不与边界上的 'O' 相连的 'O' 最终都会被
+//填充为 'X'。如果两个元素在水平或垂直方向相邻，则称它们是“相连”的。 
+// Related Topics 深度优先搜索 广度优先搜索 并查集 
+// 👍 326 👎 0
+
+ 
+package leetcode.editor.cn;
+//Java：被围绕的区域
+public class P130SurroundedRegions{
+    public static void main(String[] args) {
+        Solution solution = new P130SurroundedRegions().new Solution();
+        // TO TEST
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    //解法1. DFS/BFS
+    //这里使用DFS
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) return;
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // 从边缘o开始搜索
+                boolean isEdge = i == 0 || j == 0 || i == m - 1 || j == n - 1;
+                if (isEdge && board[i][j] == 'O') {
+                    dfs(board, i, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    public void dfs(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length  || j >= board[0].length || board[i][j] == 'X' || board[i][j] == '#') {
+            // board[i][j] == '#' 说明已经搜索过了.
+            return;
+        }
+        board[i][j] = '#';
+        dfs(board, i - 1, j); // 上
+        dfs(board, i + 1, j); // 下
+        dfs(board, i, j - 1); // 左
+        dfs(board, i, j + 1); // 右
+    }
+
+
+    //解法2. 并查集
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
