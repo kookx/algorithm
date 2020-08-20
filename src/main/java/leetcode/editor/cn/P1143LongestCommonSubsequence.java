9bd3cc70566068905b159700b12b21853934d5d8
kookx <onekook.me@gmail.com>
2020-08-20 16:32:37 +0800

    update code

--- src/main/java/leetcode/editor/cn/P1143LongestCommonSubsequence.java
+++ src/main/java/leetcode/editor/cn/P1143LongestCommonSubsequence.java
@@ -64,8 +64,8 @@ class Solution {
         int[][] dp = new int[n + 1][m + 1];
         // base case
         // 可以省略，初始化默认为0
-//        for (int i = 0; i < m; ++i) dp[0][i] = 0;
-//        for (int i = 0; i < n; ++i) dp[i][0] = 0;
+//        for (int i = 0; i < m + 1; ++i) dp[0][i] = 0;
+//        for (int i = 0; i < n + 1; ++i) dp[i][0] = 0;
         for (int i = 1; i < n + 1; ++i) {
             for (int j = 1; j < m + 1; ++j) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
