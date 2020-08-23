e6c7a79997d082ab9830d6d0e9c46294748e2d60
kookx <onekook.me@gmail.com>
2020-08-23 23:39:33 +0800

    $1 #300.最长上升子序列 @算法训练营课后作业 &动态规划

---
+++ src/main/java/leetcode/editor/cn/P300LongestIncreasingSubsequence.java
@@ -0,0 +1,54 @@
+//给定一个无序的整数数组，找到其中最长上升子序列的长度。 
+//
+// 示例: 
+//
+// 输入: [10,9,2,5,3,7,101,18]
+//输出: 4 
+//解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。 
+//
+// 说明: 
+//
+// 
+// 可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。 
+// 你算法的时间复杂度应该为 O(n2) 。 
+// 
+//
+// 进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗? 
+// Related Topics 二分查找 动态规划 
+// 👍 925 👎 0
+
+ 
+package leetcode.editor.cn;
+//Java：最长上升子序列
+public class P300LongestIncreasingSubsequence{
+    public static void main(String[] args) {
+        Solution solution = new P300LongestIncreasingSubsequence().new Solution();
+        // TO TEST
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+public class Solution {
+    //解法：动态规划
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < dp.length; i++) {
+            int maxval = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    maxval = Math.max(maxval, dp[j]);
+                }
+            }
+            dp[i] = maxval + 1;
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
