b379403551686e2927bc26764d51b9a4decc4918
kookx <onekook.me@gmail.com>
2020-08-26 14:55:06 +0800

    $2 #122.买卖股票的最佳时机 II @力扣探索卡片-初级算法-数组 &贪心算法、动态规划

--- src/main/java/leetcode/editor/cn/P122BestTimeToBuyAndSellStockIi.java
+++ src/main/java/leetcode/editor/cn/P122BestTimeToBuyAndSellStockIi.java
@@ -53,7 +53,7 @@ public class P122BestTimeToBuyAndSellStockIi{
 class Solution {
 
     public int maxProfit(int[] prices) {
-        return greedy(prices);
+        return dp1(prices);
     }
 
     // 解法1. 贪心算法
@@ -68,6 +68,37 @@ class Solution {
             }
         }
         return res;
+    }
+
+    // 解法2. 动态规划
+    // 状态定义：dp[i][0]表示第i天卖出股票的最大收益，dp[i][1]表示第i天持有股票的最大收益
+    private int dp(int[] prices) {
+        int len = prices.length;
+        int[][] dp = new int[len][2];
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[len - 1][0];
+    }
+
+    // 解法3. 动态规划状态压缩
+    private int dp1(int[] prices) {
+        int len = prices.length;
+        if (len < 2)
+            return 0;
+        int hold = -prices[0];
+        int cash = 0;
+        int preHold = hold;
+        int preCash = cash;
+        for (int i = 1; i < len; i++) {
+            cash = Math.max(preCash, preHold + prices[i]);
+            hold = Math.max(preHold, preCash - prices[i]);
+            preCash = cash;
+            preHold = hold;
+        }
+        return cash;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
