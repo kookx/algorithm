d40d98706b2cc3b533ce2c05c1501fc2d8456fe4
kookx <onekook.me@gmail.com>
2020-09-05 22:04:04 +0800

    $1 #60.第k个排列 @每日一题 $DFS、回溯

---
+++ src/main/java/leetcode/editor/cn/P60PermutationSequence.java
@@ -0,0 +1,124 @@
+//给出集合 [1,2,3,…,n]，其所有元素共有 n! 种排列。 
+//
+// 按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下： 
+//
+// 
+// "123" 
+// "132" 
+// "213" 
+// "231" 
+// "312" 
+// "321" 
+// 
+//
+// 给定 n 和 k，返回第 k 个排列。 
+//
+// 说明： 
+//
+// 
+// 给定 n 的范围是 [1, 9]。 
+// 给定 k 的范围是[1, n!]。 
+// 
+//
+// 示例 1: 
+//
+// 输入: n = 3, k = 3
+//输出: "213"
+// 
+//
+// 示例 2: 
+//
+// 输入: n = 4, k = 9
+//输出: "2314"
+// 
+// Related Topics 数学 回溯算法 
+// 👍 367 👎 0
+
+ 
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+
+//Java：第k个排列
+public class P60PermutationSequence{
+    public static void main(String[] args) {
+        Solution solution = new P60PermutationSequence().new Solution();
+        // TO TEST
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    // 解法1：DFS回溯+剪枝
+    /**
+     * 记录数字是否使用过
+     */
+    private boolean[] used;
+
+    /**
+     * 阶乘数组
+     */
+    private int[] factorial;
+
+    private int n;
+    private int k;
+
+    public String getPermutation(int n, int k) {
+        this.n = n;
+        this.k = k;
+        calculateFactorial(n);
+
+        // 查找全排列需要的布尔数组
+        used = new boolean[n + 1];
+        Arrays.fill(used, false);
+
+        StringBuilder path = new StringBuilder();
+        dfs(0, path);
+        return path.toString();
+    }
+
+
+    /**
+     * @param index 在这一步之前已经选择了几个数字，其值恰好等于这一步需要确定的下标位置
+     * @param path
+     */
+    private void dfs(int index, StringBuilder path) {
+        if (index == n) {
+            return;
+        }
+
+        // 计算还未确定的数字的全排列的个数，第 1 次进入的时候是 n - 1
+        int cnt = factorial[n - 1 - index];
+        for (int i = 1; i <= n; i++) {
+            if (used[i]) {
+                continue;
+            }
+            if (cnt < k) {
+                k -= cnt;
+                continue;
+            }
+            path.append(i);
+            used[i] = true;
+            dfs(index + 1, path);
+            // 注意 1：没有回溯（状态重置）的必要
+
+            // 注意 2：这里要加 return，后面的数没有必要遍历去尝试了
+            return;
+        }
+    }
+
+    /**
+     * 计算阶乘数组
+     *
+     * @param n
+     */
+    private void calculateFactorial(int n) {
+        factorial = new int[n + 1];
+        factorial[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
