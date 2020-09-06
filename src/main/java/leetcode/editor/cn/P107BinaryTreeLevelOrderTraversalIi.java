e06ae6958be12cbceddd6047e71af65458b8aebe
kookx <onekook.me@gmail.com>
2020-09-06 23:21:01 +0800

    $1 #107.二叉树的层次遍历 II @每日一题 $树、BFS

---
+++ src/main/java/leetcode/editor/cn/P107BinaryTreeLevelOrderTraversalIi.java
@@ -0,0 +1,76 @@
+//给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历） 
+//
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其自底向上的层次遍历为： 
+//
+// [
+//  [15,7],
+//  [9,20],
+//  [3]
+//]
+// 
+// Related Topics 树 广度优先搜索 
+// 👍 322 👎 0
+
+ 
+package leetcode.editor.cn;
+
+import java.util.*;
+
+//Java：二叉树的层次遍历 II
+public class P107BinaryTreeLevelOrderTraversalIi{
+    public static void main(String[] args) {
+        Solution solution = new P107BinaryTreeLevelOrderTraversalIi().new Solution();
+        // TO TEST
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // 解法1：BFS
+    // 思路1：顺序BFS+逆序输出
+    // 思路2：顺序BFS+链表头部插入
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; ++i) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            // 在链表头部插入
+            res.add(0, level);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
