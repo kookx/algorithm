a5bc67acfd207fea765a0085a111e0ae755eda04
kookx <onekook.me@gmail.com>
2020-09-09 16:05:51 +0800

    $1 #16.最接近的三数之和 @随机刷题 &数组、双指针

---
+++ src/main/java/leetcode/editor/cn/P16ThreeSumClosest.java
@@ -0,0 +1,64 @@
+//给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和
+//。假定每组输入只存在唯一答案。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：nums = [-1,2,1,-4], target = 1
+//输出：2
+//解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 3 <= nums.length <= 10^3 
+// -10^3 <= nums[i] <= 10^3 
+// -10^4 <= target <= 10^4 
+// 
+// Related Topics 数组 双指针 
+// 👍 564 👎 0
+
+ 
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+
+//Java：最接近的三数之和
+public class P16ThreeSumClosest{
+    public static void main(String[] args) {
+        Solution solution = new P16ThreeSumClosest().new Solution();
+        // TO TEST
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    // 解法1：排序+双指针
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int ans = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length; ++i) {
+            int start = i + 1, end = nums.length - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (Math.abs(target - sum) < Math.abs(target - ans)) {
+                    ans = sum;
+                }
+                if (sum > target) {
+                    end--;
+                } else if (sum < target) {
+                    start++;
+                } else {
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
