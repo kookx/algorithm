e5740628470b1e55e60d34ab509dd13263ac3978
kookx <onekook.me@gmail.com>
2020-09-14 16:13:56 +0800

    $3 #94.二叉树的中序遍历 @每日一题 $二叉树、DFS

--- src/main/java/leetcode/editor/cn/P94BinaryTreeInorderTraversal.java
+++ src/main/java/leetcode/editor/cn/P94BinaryTreeInorderTraversal.java
@@ -39,8 +39,8 @@ public class P94BinaryTreeInorderTraversal{
  * }
  */
 class Solution {
-    // 解法：dfs，手动模拟栈
-    public List<Integer> inorderTraversal(TreeNode root) {
+    // 解法1：dfs，手动模拟栈
+    public List<Integer> inorderTraversal1(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if (root == null) return res;
         Deque<TreeNode> stack = new ArrayDeque<>();
@@ -53,6 +53,37 @@ class Solution {
             cur = stack.pop();
             res.add(cur.val);
             cur = cur.right;
+        }
+        return res;
+    }
+    // 解法2：Morris遍历，O(1)时间复杂度
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        TreeNode predecessor = null;
+        while (root != null) {
+            if (root.left != null) {
+                // predecessor 节点就是当前 root 节点向左走一步，然后一直向右走至无法走为止
+                predecessor = root.left;
+                while (predecessor.right != null && predecessor.right != root) {
+                    predecessor = predecessor.right;
+                }
+                // 让 predecessor 的右指针指向 root，继续遍历左子树
+                if (predecessor.right == null) {
+                    predecessor.right = root;
+                    root = root.left;
+                }
+                // 说明左子树已经访问完了，我们需要断开链接
+                else {
+                    res.add(root.val);
+                    root = root.right;
+                    predecessor.right = null;
+                }
+            }
+            // 如果没有左孩子，则直接访问右孩子
+            else {
+                res.add(root.val);
+                root = root.right;
+            }
         }
         return res;
     }
