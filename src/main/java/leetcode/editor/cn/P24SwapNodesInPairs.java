c6ff97547edd22e88c3d3f36b342ee051fe48377
kookx <onekook.me@gmail.com>
2020-10-12 23:19:25 +0800

    $2 #24.两两交换链表中的节点 @算法训练营刷题打卡 &链表、递归

--- src/main/java/leetcode/editor/cn/P24SwapNodesInPairs.java
+++ src/main/java/leetcode/editor/cn/P24SwapNodesInPairs.java
@@ -38,31 +38,14 @@ public class P24SwapNodesInPairs{
  * }
  */
 class Solution {
+    // 解法：递归
     public ListNode swapPairs(ListNode head) {
-        // 迭代解法
-        ListNode pre = new ListNode(0);
-        pre.next = head;
-        ListNode temp = pre;
-        // 链表代码注意几个点： 保持清醒，别被绕晕
-        //   ①如果设置两个引用(指针)相等，若一个引用改变了该引用里的某个变量对象的引用，另一个引用同时改变，因为两个引用指向同一个地址
-        //   ②如果设置两个引用(指针)相等，若一个引用改变了自身的引用，则不影响另一个引用，他们变成指向不同地址的引用
-        // 总体思路就是：1.将上次交换后的节点指向这次交换后的节点。这一步可以放在2、3前也可以放在2、3后，不影响
-        //              2.将这次交换前的节点指向这次交换后的节点的后继节点。
-        //              3.将这次交换后的节点指向这次交换前的节点。
-        while (temp.next != null && temp.next.next != null) {
-            //保存开始节点和结束节点
-            ListNode start = temp.next;
-            ListNode end = temp.next.next;
-            //该循环第一次执行时temp=pre，目的是为了设置交换后的链表头结点，后续循环temp.next=end意思就是将上次start的next指向当前的end
-            temp.next = end;
-            //将开始节点的next指针指向end之后的节点
-            start.next = end.next;
-            //将结束节点的next指针指向开始节点，调换位置
-            end.next = start;
-            //将临时节点置为新的开始节点
-            temp = start;
-        }
-        return pre.next;
+        if (head == null || head.next == null) return head;
+
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
